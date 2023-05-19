65c2994c5bdf47494f604e6939f304faf4306237
ray837 <59028643+ray837@users.noreply.github.com>
2023-05-19 16:12:54 +0530

    Update sub.js

--- src/component/sub.js
+++ src/component/sub.js
@@ -1,6 +1,10 @@
 import React, { useEffect ,useState} from "react";
-
+import {  useParams } from "react-router-dom";
 export default function Sub() {
+    const { id } = useParams(); 
+ 
+
     return(<>
-    
+    <h3>submitted application <span style={{color:"green"}}>Successfully</span> </h3>
+    <h2>{id}</h2>
  </>);}
